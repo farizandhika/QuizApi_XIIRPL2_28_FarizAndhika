867a8d5291733e4f7aaa83b94ae1ef358bbbbdb7
farizandhika <129804040+farizandhika@users.noreply.github.com>
2024-01-11 04:33:29 +0700

    Bab H

--- router/jobsheet.js
+++ router/jobsheet.js
@@ -1,7 +1,22 @@
-const jobsheetController = require('../controllers/jobsheet');
-const router = require('express').Router();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+const quizRoute = require('./router/quiz');
+const jobsheetRoute = require('./router/jobsheet');
 
-router.post('/one', jobsheetController.submitOne);
-router.post('/many', jobsheetController.submitMany);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-module.exports = router;
+const db = require('./models')
+db.sequelize.sync()
+
+app.get('/', (req, res) => {
+    res.send('Quiz ExpressJS API by Dhika');
+});
+
+app.use('/api/quizzes', quizRoute)
+app.use('/api/jobsheet', jobsheetRoute)
+
+app.listen(port, () => console.log(`App listening on port http://localhost:${port}!`));
